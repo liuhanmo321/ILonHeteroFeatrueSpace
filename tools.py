02236dce99a7e400c383bcb880ab8d3a959b54f3
liuhanmo321 <czhanmo@gmail.com>
2022-04-17 19:54:16 +0800

    Cleaned Code

--- tools.py
+++ tools.py
@@ -8,13 +8,13 @@ from torch.autograd import Variable
 
 
 
-def make_default_mask(x):
-    mask = np.ones_like(x)
-    mask[:,-1] = 0
-    return mask
+# def make_default_mask(x):
+#     mask = np.ones_like(x)
+#     mask[:,-1] = 0
+#     return mask
 
-def tag_gen(tag,y):
-    return np.repeat(tag,len(y['data']))
+# def tag_gen(tag,y):
+#     return np.repeat(tag,len(y['data']))
 
 
 def count_parameters(model):
@@ -27,79 +27,6 @@ def get_scheduler(args, optimizer):
         scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
                                       milestones=[args.epochs // 2.667, args.epochs // 1.6, args.epochs // 1.142], gamma=0.1)
     return scheduler
-
-# def imputations_acc_justy(model,dloader,device):
-#     model.eval()
-#     m = nn.Softmax(dim=1)
-#     y_test = torch.empty(0).to(device)
-#     y_pred = torch.empty(0).to(device)
-#     prob = torch.empty(0).to(device)
-#     with torch.no_grad():
-#         for i, data in enumerate(dloader, 0):
-#             x_categ, x_cont, cat_mask, con_mask = data[0].to(device), data[1].to(device),data[2].to(device),data[3].to(device)
-#             _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, cat_mask, con_mask,model)
-#             reps = model.transformer(x_categ_enc, x_cont_enc)
-#             y_reps = reps[:,model.num_categories-1,:]
-#             y_outs = model.mlpfory(y_reps)
-#             # import ipdb; ipdb.set_trace()   
-#             y_test = torch.cat([y_test,x_categ[:,-1].float()],dim=0)
-#             y_pred = torch.cat([y_pred,torch.argmax(m(y_outs), dim=1).float()],dim=0)
-#             prob = torch.cat([prob,m(y_outs)[:,-1].float()],dim=0)
-     
-#     correct_results_sum = (y_pred == y_test).sum().float()
-#     acc = correct_results_sum/y_test.shape[0]*100
-#     auc = roc_auc_score(y_score=prob.cpu(), y_true=y_test.cpu())
-#     return acc, auc
-
-
-# def multiclass_acc_justy(model,dloader,device):
-#     model.eval()
-#     vision_dset = True
-#     m = nn.Softmax(dim=1)
-#     y_test = torch.empty(0).to(device)
-#     y_pred = torch.empty(0).to(device)
-#     prob = torch.empty(0).to(device)
-#     with torch.no_grad():
-#         for i, data in enumerate(dloader, 0):
-#             x_categ, x_cont = data[0].to(device), data[1].to(device)
-#             _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, model)
-#             reps = model.transformer(x_categ_enc, x_cont_enc)
-#             y_reps = reps[:,model.num_categories-1,:]
-#             y_outs = model.mlpfory(y_reps)
-#             # import ipdb; ipdb.set_trace()   
-#             y_test = torch.cat([y_test,x_categ[:,-1].float()],dim=0)
-#             y_pred = torch.cat([y_pred,torch.argmax(m(y_outs), dim=1).float()],dim=0)
-     
-#     correct_results_sum = (y_pred == y_test).sum().float()
-#     acc = correct_results_sum/y_test.shape[0]*100
-#     return acc, 0
-
-
-# def classification_scores(model, dloader, device, task,vision_dset):
-#     model.eval()
-#     m = nn.Softmax(dim=1)
-#     y_test = torch.empty(0).to(device)
-#     y_pred = torch.empty(0).to(device)
-#     prob = torch.empty(0).to(device)
-#     with torch.no_grad():
-#         for i, data in enumerate(dloader, 0):
-#             x_categ, x_cont, y_gts = data[0].to(device), data[1].to(device),data[2].to(device)
-#             _ , x_categ_enc, x_cont_enc = embed_data_mask(x_categ, x_cont, model)           
-#             reps = model.transformer(x_categ_enc, x_cont_enc)
-#             y_reps = reps[:,0,:]
-#             y_outs = model.mlpfory(y_reps)
-#             # import ipdb; ipdb.set_trace()   
-#             y_test = torch.cat([y_test,y_gts.squeeze().float()],dim=0)
-#             y_pred = torch.cat([y_pred,torch.argmax(y_outs, dim=1).float()],dim=0)
-#             if task == 'binary':
-#                 prob = torch.cat([prob,m(y_outs)[:,-1].float()],dim=0)
-     
-#     correct_results_sum = (y_pred == y_test).sum().float()
-#     acc = correct_results_sum/y_test.shape[0]*100
-#     auc = 0
-#     if task == 'binary':
-#         auc = roc_auc_score(y_score=prob.cpu(), y_true=y_test.cpu())
-#     return acc.cpu().numpy(), auc
 
 def classification_scores_cont(model, dloader, device, task, data_id):
     model.eval()
@@ -153,39 +80,6 @@ def classification_scores_specific_only(model, dloader, device, task, data_id):
     if task == 'binary':
         auc = roc_auc_score(y_score=prob.cpu(), y_true=y_test.cpu())
     return acc.cpu().numpy(), auc
-
-# def classification_scores_ours(model, dloader, device, task, data_id, alpha):
-#     model.eval()
-#     m = nn.Softmax(dim=1)
-#     y_test = torch.empty(0).to(device)
-#     y_pred = torch.empty(0).to(device)
-#     prob = torch.empty(0).to(device)
-#     with torch.no_grad():
-#         for i, data in enumerate(dloader, 0):
-#             x_categ, x_cont, y_gts = data[0].to(device), data[1].to(device),data[2].to(device)
-#             _ , x_categ_enc, x_cont_enc = embed_data_cont(x_categ, x_cont, model, data_id)
-
-#             shared_feature = model.shared_extractor(x_categ_enc, x_cont_enc)[:,0,:]
-#             shared_output = model.shared_classifier[data_id](shared_feature)
-#             shared_p = torch.softmax(shared_output, dim=1)
-
-#             specific_feature = model.specific_extractor[data_id](x_categ_enc, x_cont_enc)[:,0,:]
-#             specific_output = model.specific_classifier[data_id](specific_feature)
-#             specific_p = torch.softmax(specific_output, dim=1)
-            
-#             y_outs = alpha * shared_p + (1-alpha) * specific_p
-#             # import ipdb; ipdb.set_trace()   
-#             y_test = torch.cat([y_test,y_gts.squeeze().float()],dim=0)
-#             y_pred = torch.cat([y_pred,torch.argmax(y_outs, dim=1).float()],dim=0)
-#             if task == 'binary':
-#                 prob = torch.cat([prob, m(y_outs)[:,-1].float()],dim=0)
-     
-#     correct_results_sum = (y_pred == y_test).sum().float()
-#     acc = correct_results_sum/y_test.shape[0]*100
-#     auc = 0
-#     if task == 'binary':
-#         auc = roc_auc_score(y_score=prob.cpu(), y_true=y_test.cpu())
-#     return acc.cpu().numpy(), auc
 
 def classification_scores_ours(model, dloader, device, task, data_id, alpha):
     model.eval()
@@ -349,29 +243,6 @@ def classification_scores_pnn(model, dloader, device, task, data_id):
             x_categ, x_cont, y_gts = data[0].to(device), data[1].to(device),data[2].to(device)
             _ , x_categ_enc, x_cont_enc = embed_data_cont(x_categ, x_cont, model, data_id)           
             y_outs = model.forward(x_categ_enc, x_cont_enc, data_id)
-            y_test = torch.cat([y_test,y_gts.squeeze().float()],dim=0)
-            y_pred = torch.cat([y_pred,torch.argmax(y_outs, dim=1).float()],dim=0)
-            if task == 'binary':
-                prob = torch.cat([prob,m(y_outs)[:,-1].float()],dim=0)
-     
-    correct_results_sum = (y_pred == y_test).sum().float()
-    acc = correct_results_sum/y_test.shape[0]*100
-    auc = 0
-    if task == 'binary':
-        auc = roc_auc_score(y_score=prob.cpu(), y_true=y_test.cpu())
-    return acc.cpu().numpy(), auc
-
-def classification_scores_hat(model, dloader, device, task, data_id, s=400):
-    model.eval()
-    m = nn.Softmax(dim=1)
-    y_test = torch.empty(0).to(device)
-    y_pred = torch.empty(0).to(device)
-    prob = torch.empty(0).to(device)
-    with torch.no_grad():
-        for i, data in enumerate(dloader, 0):
-            x_categ, x_cont, y_gts = data[0].to(device), data[1].to(device),data[2].to(device)
-            _ , x_categ_enc, x_cont_enc = embed_data_cont(x_categ, x_cont, model, data_id)           
-            y_outs, _ = model.forward(x_categ_enc, x_cont_enc, data_id, s=s)
             y_test = torch.cat([y_test,y_gts.squeeze().float()],dim=0)
             y_pred = torch.cat([y_pred,torch.argmax(y_outs, dim=1).float()],dim=0)
             if task == 'binary':
